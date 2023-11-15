a09b0c81db8c064fb9886b044d61ffd0f1ef1e5a
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-11-15 18:40:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+function secondHighest(arr) {
+  //Write your code here
+
+  if (arr.length < 2) {
+    return -Infinity;
+  }
+
+  let max = -Infinity,
+    secMax = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      secMax = max;
+      max = arr[i];
+    } else if (arr[i] > secMax && arr[i] < max) {
+      secMax = arr[i];
+    }
+  }
+
+  return secMax;
+}
+
+function Main() {
+  var n = prompt("Enter the number of elements");
+  var arr = [];
+  for (var i = 0; i < n; i++) {
+    arr[i] = prompt("Enter element " + (i + 1));
+  }
+  alert(secondHighest(arr));
+}
+Main();
